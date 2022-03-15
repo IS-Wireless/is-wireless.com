8df25839d947b870421d422f297eb06d8179bd32
Kamil <kamil@webo.agency>
2022-03-15 18:14:50 +0100

    cta job vue file

---
+++ components/cta-job.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="w-full bg-cover bg-fixed bg-gray-light py-[100px]" :style="{backgroundImage: 'url(' + backgroundUrl + ')'}">
+        <div class="flex flex-col justify-center items-center w-4/5 mx-auto">
+            <h2 class="text-white mb-5">{{title}}</h2>
+            <hr class=" w-[100px] h-0 border-0 border-t-4 border-white mb-5">
+            <slot></slot>
+        </div>
+    </section>
+  
+</template>
+
+<script>
+export default {
+    name: 'CtaJobs',
+    props:{
+        title:{
+            type: String,
+            required: true,
+            default: 'CTA Title'
+        },
+        backgroundUrl:{
+            type:String,
+            required: true
+      }
+    }
+
+}
+</script>
+
+<style>
+
+</style>

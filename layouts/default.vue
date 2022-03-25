babc9ed98d7d7dfe53527edae0c3753f65aedbc6
Kamil <kamil@webo.agency>
2022-03-25 14:26:01 +0100

    side menu template data and search bars

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Navbar :menu="generalData.menu" />
+    <Navbar :mainMenu="mainMenuData.menu" :sideMenu="sideMenuData.menu" />
     <Nuxt />
   </div>
 </template>
@@ -14,9 +14,56 @@ export default {
     Navbar
   },
   computed: {
-      generalData() {
-      return this.$store.getters['general/getData']
+      mainMenuData() {
+        return this.$store.getters['general/getData']
       },
+
+      sideMenuData(){
+        return{
+          menu:{
+            0:{
+              title: '5G Made Together',
+              desktop: false
+            },
+            1:{
+              title: 'Academy',
+              desktop: false
+            },
+            2:{
+              title: 'Networks',
+              desktop: false
+            },
+            3:{
+              title: 'Solutions',
+              desktop: false
+            },
+            4:{
+              title: 'Research',
+              desktop: true
+            },
+            5:{
+              title: 'Resources',
+              desktop: true
+            },
+            6:{
+              title: 'About Us',
+              desktop: true
+            },
+            7:{
+              title: 'Careers',
+              desktop: true
+            },
+            8:{
+              title: 'News',
+              desktop: true
+            },
+            9:{
+              title: 'Contact',
+              desktop: true
+            }
+          }
+        }
+      }
   },
 }
 

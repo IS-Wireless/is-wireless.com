64f4a178bf43e35dae3bf070d39bf24c5dfdc7a3
Kamil <kamil@webo.agency>
2022-03-26 15:22:24 +0100

    parallax temp fix

--- components/content-section.vue
+++ components/content-section.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="relative w-full mb-8 tablet-wide:mb-14 pt-10 tablet-wide:pt-16" :class="{' bg-black/50 tablet-wide:bg-black/5 overflow-hidden':backgroundUrl}" >
     <!-- <nuxt-picture class="absolute parallax-img h-full min-w-full top-0 left-0 -z-10 transform" v-if="backgroundUrl" ref="parallax" :src="backgroundUrl"></nuxt-picture> -->
-    <div class="absolute -z-10 bottom-0 bg-[25%_center] mx-auto desktop:bg-cover transform w-full min-h-screen h-full" ref="parallax" v-if="backgroundUrl" v-bind:style="{ 'background-image': 'url(' + backgroundUrl + ')'}"></div>
+    <div class="absolute -z-10 bottom-0 bg-[25%_center] mx-auto desktop:bg-cover transform w-full min-h-screen h-[120%]" ref="parallax" v-if="backgroundUrl" v-bind:style="{ 'background-image': 'url(' + backgroundUrl + ')'}"></div>
     <div class="flex flex-col tablet-wide:flex-row w-4/5 mx-auto ">
       <div class="tablet-wide:flex-[1_1_50%] tablet-wide:mr-5">
         <slot name="left"></slot>
@@ -25,7 +25,7 @@ export default {
     methods: {
       handleParallax() {
         var element = this.$refs.parallax;
-        element.style.transform = 'translateY(' +  (((window.top.scrollY - (this.$el.clientTop - this.$el.clientHeight/2) )/100)) +'%)';
+        element.style.transform = 'translateY(' +  (((window.top.scrollY - (this.$el.clientTop + this.$el.clientHeight) )/100)) +'%)';
       },
     },
     mounted() {

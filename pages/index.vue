2b436333daf6cf268a26adfc9eb914009c412c5a
Kamil <kamil@webo.agency>
2022-03-26 14:48:53 +0100

    data fetching temp disabled, data moved to store

--- pages/index.vue
+++ pages/index.vue
@@ -6,17 +6,12 @@
       <template v-slot:left>
         <SectionHeader
           :class="'text-gray-main'"
-          :title="'Complete E2E European Open RAN'"
+          :title="frontPageData.acf.E2e.title"
         />
         <p class="mb-14 text-gray-light">
-          Software-hardware decoupling and opening of interfaces is a phenomenon
-          well known from other domains such as computers or mobile phones. It
-          always brings more choice at a lower price. This is the long-awaited
-          paradigm shift in RAN business models. On the contrary, single-RAN
-          promoted by the “traditional” vendors is the main reason and cause for
-          the telco industry standstill.
+          {{ frontPageData.acf.E2e.text }}
         </p>
-        <List :class="'text-gray-main'" :items="testList" />
+        <List :class="'text-gray-main'" :items="frontPageData.acf.E2e.list" />
       </template>
 
       <template v-slot:right>
@@ -25,18 +20,16 @@
         />
       </template>
     </ContentSection>
-    <ContentSection :class="'text-white'" :backgroundUrl="bgUrlRadio">
+    
+    <ContentSection :class="'text-white'" :backgroundUrl="frontPageData.acf.granularized.bgUrl">
       <template v-slot:right>
         <SectionHeader
-          :title="'Highly granularized Network Function Virtualization'"
+          :title="frontPageData.acf.granularized.title"
         />
         <p class="mb-14 text-inherit">
-          Decoupling happens also inside the software. Instead of virtual
-          software monoliths we are using libraries of highly granularized
-          Virtual Network Functions. Moreover, they can be engineered in
-          semi-automatic mode.
+          {{ frontPageData.acf.granularized.text }}
         </p>
-        <List :items="granularizedList" />
+        <List :items="frontPageData.acf.granularized.list" />
       </template>
     </ContentSection>
 
@@ -44,16 +37,12 @@
       <template v-slot:left>
         <SectionHeader
           :class="'text-gray-main'"
-          :title="'Radio Resource Management'"
+          :title="frontPageData.acf.radio.title"
         />
         <p class="mb-14 text-gray-light">
-          Future networks will need to be much denser to meet the 1000x capacity
-          requirement. It may mean the deployment of low-power Radio Units at
-          every corner of the street and in every room. RRM algorithms run by
-          RIC can handle interference in such situations. Moreover, they can
-          manage QoS both in ultra-dense and in macro networks.
+          {{frontPageData.acf.radio.text}}
         </p>
-        <List :class="'text-gray-main'" :items="radioList" />
+        <List :class="'text-gray-main'" :items="frontPageData.acf.radio.list" />
       </template>
 
       <template v-slot:right>
@@ -63,15 +52,28 @@
       </template>
     </ContentSection>
     <CtaJob
-      :title="'Check out job openings'"
-      :backgroundUrl="'https://www.is-wireless.com/wp-content/uploads/2021/02/5G-job-IS-Wireless.jpg'"
+      :title="frontPageData.acf.cta_job.title"
+      :backgroundUrl="frontPageData.acf.cta_job.bgUrl"
     >
       <template>
         <p class="text-white text-center mb-5">
-          IS-Wireless is always looking for talented people!
+          {{frontPageData.acf.cta_job.text}}
         </p>
         <a
-          class="block object-cover object- text-sm text-white uppercase px-7 py-3 rounded-full bg-blue-main hover:bg-white hover:text-black duration-300 tablet:mb-0 mb-6"
+          class="
+            block
+            object-cover object-
+            text-sm text-white
+            uppercase
+            px-7
+            py-3
+            rounded-full
+            bg-blue-main
+            hover:bg-white hover:text-black
+            duration-300
+            tablet:mb-0
+            mb-6
+          "
           href="https://www.is-wireless.com/networks/"
           >Explore careers</a
         >
@@ -102,82 +104,19 @@ export default Vue.extend({
     CtaJob,
   },
 
-  async fetch({ app, store }) {
-    await app.$wp
-      .namespace('wuxt')
-      .v1()
-      .frontPage()
-      .then(function (data) {
-        store.commit('homepage/save', data)
-      })
-  },
+  // async fetch({ app, store }) {
+  //   await app.$wp
+  //     .namespace('wuxt')
+  //     .v1()
+  //     .frontPage()
+  //     .then(function (data) {
+  //       store.commit('homepage/save', data)
+  //     })
+  // },
   computed: {
     frontPageData() {
       return this.$store.getters['homepage/getData']
     },
-  },
-  data() {
-    return {
-      testList: [
-        {
-          text:
-            'Savings of ~30% in Capex and ~40% in Opex when compared to “traditional” RAN deployment already demonstrated by Rakuten',
-          iconUrl: '../static/test-icon.png',
-        },
-        {
-          text:
-            '	No vendor lock-in and rich supplier ecosystem significantly lowering business risks such as dependency on a single supplier',
-          iconUrl: '../static/test-icon.png',
-        },
-        {
-          text:
-            '	Better security and scalability by use of independently managed network functions deployed when needed',
-          iconUrl:
-            'https://e7.pngegg.com/pngimages/527/437/png-clipart-wifi-logo-wi-fi-computer-icons-wireless-symbol-wifi-icon-angle-electronics.png',
-        },
-      ],
-
-      granularizedList: [
-        {
-          text: 'Possibility to support any of the protocol splits',
-          iconUrl: '../static/test-icon.png',
-        },
-        {
-          text:
-            'Ease of deployment on any computing resources including dedicated and shared ones',
-          iconUrl: '../static/test-icon.png',
-        },
-        {
-          text:
-            'Significantly shorter time to market in new feature deployment',
-          iconUrl:
-            'https://e7.pngegg.com/pngimages/527/437/png-clipart-wifi-logo-wi-fi-computer-icons-wireless-symbol-wifi-icon-angle-electronics.png',
-        },
-      ],
-
-      radioList: [
-        {
-          text:
-            'Constructive use of interference, radiation lowering, energy efficiency',
-          iconUrl: '../static/test-icon.png',
-        },
-        {
-          text:
-            'Practical realization of ultra-dense and high capacity networks',
-          iconUrl: '../static/test-icon.png',
-        },
-        {
-          text:
-            'Ultra-reliable and low-latency communications enabled by the RRM algorithms',
-          iconUrl:
-            'https://e7.pngegg.com/pngimages/527/437/png-clipart-wifi-logo-wi-fi-computer-icons-wireless-symbol-wifi-icon-angle-electronics.png',
-        },
-      ],
-
-      bgUrlRadio:
-        'https://www.is-wireless.com/wp-content/uploads/2021/02/RRH-1.jpg',
-      bgUrlJobs: '',
-    }
   },
 })
 </script>
--- store/homepage.js
+++ store/homepage.js
@@ -3,11 +3,92 @@ import { isEmpty as _isEmpty } from 'lodash'
 const filterWords = ['yoast_head', 'meta', '{}']
 
 export const state = () => ({
-  acf:{
-    "baner_slide":[{"baner_image":"https://www.is-wireless.com/wp-content/uploads/2021/02/Indoor-capacity.jpg","baner_content":[{"acf_fc_layout":"Podstawowy","logo":"https://www.is-wireless.com/wp-content/uploads/2021/02/IS-Wireless-5GMadeTogether.svg","text":"4G and 5G Mobile Networks of the Future","button_first_text":"https://www.is-wireless.com/networks/","button_second_text":"Academy","button_second_url":"https://www.is-wireless.com/academy/"}]}]    
+  acf: {
+    baner_slide: [
+      {
+        baner_image:
+          'https://www.is-wireless.com/wp-content/uploads/2021/02/Indoor-capacity.jpg',
+        baner_content: [
+          {
+            acf_fc_layout: 'Podstawowy',
+            logo:
+              'https://www.is-wireless.com/wp-content/uploads/2021/02/IS-Wireless-5GMadeTogether.svg',
+            text: '4G and 5G Mobile Networks of the Future',
+            button_first_text: 'https://www.is-wireless.com/networks/',
+            button_second_text: 'Academy',
+            button_second_url: 'https://www.is-wireless.com/academy/',
+          },
+        ],
+      },
+    ],
+    E2e: {
+      title:'Complete E2E European Open RAN',
+      text: 'Software-hardware decoupling and opening of interfaces is a phenomenon well known from other domains such as computers or mobile phones. It always brings more choice at a lower price. This is the long-awaited paradigm shift in RAN business models. On the contrary, single-RAN promoted by the “traditional” vendors is the main reason and cause for the telco industry standstill.',
+      list: [
+        {
+          text: 'Savings of ~30% in Capex and ~40% in Opex when compared to “traditional” RAN deployment already demonstrated by Rakuten',
+          iconUrl: '../static/test-icon.png',
+        },
+        {
+          text: '	No vendor lock-in and rich supplier ecosystem significantly lowering business risks such as dependency on a single supplier',
+          iconUrl: '../static/test-icon.png',
+        },
+        {
+          text: '	Better security and scalability by use of independently managed network functions deployed when needed',
+          iconUrl:
+            'https://e7.pngegg.com/pngimages/527/437/png-clipart-wifi-logo-wi-fi-computer-icons-wireless-symbol-wifi-icon-angle-electronics.png',
+        },
+      ],
+    },
+
+    granularized: {
+      title:'Highly granularized Network Function Virtualization',
+      text: 'Decoupling happens also inside the software. Instead of virtual software monoliths we are using libraries of highly granularized Virtual Network Functions. Moreover, they can be engineered in semi-automatic mode.',
+      list: [
+        {
+          text: 'Possibility to support any of the protocol splits',
+          iconUrl: '../static/test-icon.png',
+        },
+        {
+          text: 'Ease of deployment on any computing resources including dedicated and shared ones',
+          iconUrl: '../static/test-icon.png',
+        },
+        {
+          text: 'Significantly shorter time to market in new feature deployment',
+          iconUrl:
+            'https://e7.pngegg.com/pngimages/527/437/png-clipart-wifi-logo-wi-fi-computer-icons-wireless-symbol-wifi-icon-angle-electronics.png',
+        },
+      ],      
+      bgUrl:
+      'https://www.is-wireless.com/wp-content/uploads/2021/02/RRH-1.jpg',
+    },
+
+    radio: {
+      title:'Radio Resource Management',
+      text: 'Future networks will need to be much denser to meet the 1000x capacity requirement. It may mean the deployment of low-power Radio Units at every corner of the street and in every room. RRM algorithms run by RIC can handle interference in such situations. Moreover, they can manage QoS both in ultra-dense and in macro networks.',
+      list: [
+        {
+          text: 'Constructive use of interference, radiation lowering, energy efficiency',
+          iconUrl: '../static/test-icon.png',
+        },
+        {
+          text: 'Practical realization of ultra-dense and high capacity networks',
+          iconUrl: '../static/test-icon.png',
+        },
+        {
+          text: 'Ultra-reliable and low-latency communications enabled by the RRM algorithms',
+          iconUrl:
+            'https://e7.pngegg.com/pngimages/527/437/png-clipart-wifi-logo-wi-fi-computer-icons-wireless-symbol-wifi-icon-angle-electronics.png',
+        },
+      ],
+    },
+    cta_job:{
+      title: 'Check out job openings',
+      text: 'IS-Wireless is always looking for talented people!',
+      bgUrl: 'https://www.is-wireless.com/wp-content/uploads/2021/02/5G-job-IS-Wireless.jpg',
+    }
   },
 })
-
 
 const filterData = (obj) => {
   Object.keys(obj).forEach((key) => {

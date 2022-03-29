c1e1541ae887f0ffce01f7f0d021f3675c534f17
Kamil <kamil@webo.agency>
2022-03-29 17:19:16 +0200

    static page content component

---
+++ components/content-static.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="w-full">
+        <h1 class="mb-5 pt-14 tablet-wide:w-4/5 text-2xl tablet:text-3xl" v-if="title" >{{title}}</h1>
+        <p class="mb-5 tablet-wide:w-3/4 text-justify" v-for="(item,index) in text" :key="index">{{item}}</p>
+        <div class="flex flex-col justify-center items-center tablet-wide:w-3/4 tablet-wide:mr-10" v-for="(item,index) in image" :key="index">
+            <nuxt-picture fit="contain" class="mb-5" :src="item.url" :title="item.title" ></nuxt-picture>
+            <p class="grow-0 italic text-center mb-5" v-if="item.title">{{item.title}}</p>
+        </div>
+    <div class="w-4/5 mx-auto border-0 border-t border-gray-200 mb-14"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ContentStatic',
+    props:{
+        title:{
+            type: String,
+            required: false
+        },
+        text:{
+            type: Array,
+            required: false
+        },
+        image:{
+            type:Array,
+            required: false
+        }
+    
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- pages/networks/index.vue
+++ pages/networks/index.vue
@@ -1,8 +1,32 @@
 <template>
-    <div>
-        <StaticBanner :title="networkPageData.acf.banner.title" :backgroundUrl="networkPageData.acf.banner.bgUrl"/>
-        <Breadcrumb />
+  <div>
+    <StaticBanner
+      :title="networkPageData.acf.banner.title"
+      :backgroundUrl="networkPageData.acf.banner.bgUrl"
+    />
+    <Breadcrumb />
+    <div class="w-4/5 mx-auto">
+      <ContentStatic
+        :title="'5G Network Software and Hardware to enable networking revolution'"
+        :text="['IS-Wireless provides complete end-to-end, future-proof mobile networks covering 5G, LTE as well as IoT (NB-IoT and LTE-M) standards. In our design approach, we follow recent technical advances such as Open RAN to provide customers with flexibility and economical advantages. The network solutions cover both the RAN (Radio Access Network) part, as well as the Core and MANO (Management and Orchestration). They can be used to address any deployment use case, from Rural, through Suburban/Urban to Dense Urban including the Indoor.']"
+        :image="[{
+          url: 'https://www.is-wireless.com/wp-content/uploads/2021/02/Mobile-networks-1200x605.jpg',
+          title: null,
+        },{
+          url: 'https://www.is-wireless.com/wp-content/uploads/2021/02/Logotypy-800x101.jpg',
+          title: null,
+        }]"
+      />
+        <ContentStatic
+        :text="['At the heart of our network solutions lies Network Software reflecting all important telecom functionalities. Network Software runs on Network Hardware, which includes COTS (Commercial off-the-shelf) servers and connected Radio Units. The offering is complemented with Installation Service, as shown in Figure below.']"
+        :image="[{
+          url: 'https://www.is-wireless.com/wp-content/uploads/2021/02/Mobile-Networks-Rys.-PPT-2-800x335.jpg',
+          title: null,
+        }]"
+      />
+
     </div>
+  </div>
 </template>
 
 <script>
@@ -10,22 +34,20 @@ import Vue from 'vue'
 
 import StaticBanner from '~/components/static-banner.vue'
 import Breadcrumb from '~/components/breadcrumb.vue'
+import ContentStatic from '~/components/content-static.vue'
 
 export default Vue.extend({
-    components:{
-        StaticBanner,
-        Breadcrumb
+  components: {
+    StaticBanner,
+    Breadcrumb,
+    ContentStatic,
+  },
+  computed: {
+    networkPageData() {
+      return this.$store.getters['networks/getData']
     },
-    computed:{
-        networkPageData() {
-            return this.$store.getters['networks/getData']
-        },
-    },
-
-
+  },
 })
 </script>
 
-<style>
-
-</style>
+<style></style>

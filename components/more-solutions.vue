9ecf0727851541c6f3435ffff952b329ed180c59
Kamil <kamil@webo.agency>
2022-04-05 14:09:30 +0200

    More solutions component

---
+++ components/more-solutions.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h2 class="block text-blue-main text-2xl tablet:text-3xl mb-10">
+      More solutions
+    </h2>
+    <ul class="flex flex-col tablet-wide:flex-row tablet-wide:justify-between">
+      <li
+        class="
+          mb-2.5
+          tablet-wide:basis-1/3
+          tablet-wide:px-4
+          tablet-wide:first:px-0
+          tablet-wide:first:pr-4
+          tablet-wide:last:px-0
+          tablet-wide:last:pl-4
+        "
+        v-for="item in data"
+        :key="item.title"
+      >
+        <nuxt-link
+          class="
+            p-7
+            flex
+            rounded-md
+            border border-solid border-gray-200
+            hover:border-blue-main
+            transition
+          "
+          :to="item.url"
+        >
+          <p class="text-gray-main transition text-xl">
+            {{ item.title }}
+          </p>
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MoreSolutions',
+  props: {
+    data: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style></style>
--- pages/demo.vue
+++ pages/demo.vue
@@ -1,30 +1,38 @@
 <template>
-    <div class="w-4/5 my-52 mx-auto">
-        <OfferMenu :data="
-                [{
-                title:'Software',    
-                url:'networks/software'
-            },{
-                title:'Hardware',    
-                url:'networks/hardware'
-            },{
-                title:'Services',    
-                url:'networks/services'
-            }]"
-            />
-    </div>
+  <div class="w-4/5 my-52 mx-auto">
+    <OfferMenu :data="testLinksArray" />
+    <MoreSolutions :data="testLinksArray" />
+  </div>
 </template>
 
 <script>
-import OfferMenu from "~/components/offer-menu.vue"
+import OfferMenu from '~/components/offer-menu.vue'
+import MoreSolutions from '~/components/more-solutions.vue'
 export default {
-    name: 'Demo',
-    components:{
-        OfferMenu
+  name: 'Demo',
+  components: {
+    OfferMenu,
+    MoreSolutions,
+  },
+  data() {
+    return {
+      testLinksArray: [
+        {
+          title: 'Software',
+          url: 'networks/software',
+        },
+        {
+          title: 'Hardware',
+          url: 'networks/hardware',
+        },
+        {
+          title: 'Services',
+          url: 'networks/services',
+        },
+      ],
     }
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>

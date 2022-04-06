3eed2dc6785f3c82fb32112d3eb8199a1cbdedb8
Kamil <kamil@webo.agency>
2022-04-06 14:54:04 +0200

    table component base

---
+++ components/table.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="w-full">
+        <table>
+            <thead>
+                <tr>
+                    <th v-for="item in data.head" :key="item">
+                        {{item}}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in data.content" :key="item">
+                    <td>{{item[0]}}</td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name: 'Table',
+    components:{
+        data:{
+            type: Object,
+            required: true,
+        }
+    },
+    data(){
+        return{
+            currentSort: 'asc',
+            currentSortDir: 'asc'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

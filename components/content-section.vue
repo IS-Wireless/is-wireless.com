498106c7c915cb8ee2b7647888a8dec06df2efa6
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-04-21 11:44:30 +0200

    Fix error

--- components/content-section.vue
+++ components/content-section.vue
@@ -9,18 +9,7 @@
     <div
       v-if="backgroundUrl"
       ref="parallax"
-      class="
-        absolute
-        -z-10
-        bottom-0
-        bg-[25%_center]
-        mx-auto
-        desktop:bg-cover
-        transform
-        w-full
-        min-h-screen
-        h-[120%]
-      "
+      class="absolute -z-10 bottom-0 bg-[25%_center] mx-auto desktop:bg-cover transform w-full min-h-screen h-[120%]"
       :style="{ 'background-image': 'url(' + backgroundUrl + ')' }"
     />
     <div class="flex flex-col tablet-wide:flex-row w-4/5 mx-auto">
@@ -58,11 +47,13 @@ export default {
   methods: {
     handleParallax() {
       var element = this.$refs.parallax
-      element.style.transform =
-        'translateY(' +
-        (window.top.scrollY - (this.$el.clientTop + this.$el.clientHeight)) /
-          100 +
-        '%)'
+      if (element) {
+        element.style.transform =
+          'translateY(' +
+          (window.top.scrollY - (this.$el.clientTop + this.$el.clientHeight)) /
+            100 +
+          '%)'
+      }
     },
   },
 }

a1f434c9aed5f3b50a6318bd8feaa0f1b18b9471
Kamil <kamil@webo.agency>
2022-04-28 18:12:51 +0200

    [ISW-358] Text subpage base

---
+++ pages/_slug.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Breadcrumb />
+    <div class="w-full px-[10%]">
+      <div class="w-3/4">
+        <span class="inline-block text-gray-dark mt-14">
+          <SectionHeader :title="currentRouteName" />
+        </span>
+        <div class="mb-10"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SectionHeader from '~/components/section-header.vue'
+import Breadcrumb from '~/components/breadcrumb.vue'
+
+export default {
+  components: {
+    SectionHeader,
+    Breadcrumb,
+  },
+  computed: {
+    currentRouteName() {
+      return this.$route.fullPath.slice(1, -1).split('/').slice(-1)[0]
+    },
+  },
+}
+</script>
+
+<style lang="postcss" scoped></style>

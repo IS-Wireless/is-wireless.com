b573948e894e1460bb0a5ca2cf6ab3cb5f4c2445
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-05-20 11:37:35 +0200

    Animation cross-fade add

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/main.css'],
+  css: ['@/assets/main.css', '@/assets/animation.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [

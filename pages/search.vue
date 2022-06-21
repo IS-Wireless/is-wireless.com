56fd75cedce640370241194787610cfee76e88d4
Kamil <kamil@webo.agency>
2022-06-21 16:13:41 +0200

    search base

--- pages/search.vue
+++ pages/search.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="relative z-10 flex flex-col tablet-wide:w-4/5 mx-auto">
-    <div class="gcse-searchresults-only"></div>
+    <Breadcrumb />
+    <div class="search_wrapper">
+      <div class="gcse-searchresults-only"></div>
+    </div>
   </div>
 </template>
 <script>
+import Breadcrumb from '@/components/breadcrumb.vue'
+
 export default {
+  components: {
+    Breadcrumb,
+  },
   head() {
     return {
       title: this.title,
@@ -18,4 +26,16 @@ export default {
   },
 }
 </script>
-<style scoped></style>
+<style scoped lang="postcss">
+.search_wrapper >>> .gsc-adBlock,
+.search_wrapper >>> .gsc-positioningWrapper,
+.search_wrapper >>> .gsc-orderby-container,
+.search_wrapper >>> .gs-image-box,
+.search_wrapper >>> .gsc-url-top,
+.search_wrapper >>> .gcsc-more-maybe-branding-root {
+  @apply hidden;
+}
+.search_wrapper >>> .gsc-thumbnail-inside a.gs-title {
+  @apply text-blue-main hover:text-blue-main-hover visited:text-blue-main-hover font-lato font-normal text-lg mb-10 transition duration-200;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,40 @@
 module.exports = {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
   ],
   theme: {
     screens: {
-      'phone': '320px',
+      phone: '320px',
       'phone-wide': '480px',
-      'phablet': '560px',
+      phablet: '560px',
       'tablet-small': '640px',
-      'tablet': '768px',
+      tablet: '768px',
       'tablet-wide': '1024px',
-      'desktop': '1248px',
+      desktop: '1248px',
       'desktop-wide': '1440px',
       'full-hd': '1680px',
     },
     extend: {
-      colors:{
-        blue:{
-          main: '#00A2DF',
+      colors: {
+        blue: {
+          main: '#00A2E0',
           'main-hover': '#027FAE',
-          dark: '#2173BA'
+          dark: '#2173BA',
         },
-        gray:{
+        gray: {
           light: '#F4F6F7',
           default: '#D4D9DF',
           dark: '#616A7D',
           darkest: '#111111',
-        }
+        },
       },
-      fontFamily:{
-        'lato': ['Lato','ui-serif']
-      }
+      fontFamily: {
+        lato: ['Lato', 'ui-serif'],
+      },
     },
   },
   plugins: [],

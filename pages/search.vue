2141466e88ff478830a91aa876029151ec836c4c
Kamil <kamil@webo.agency>
2022-06-21 17:00:58 +0200

    mobile styles text

--- pages/search.vue
+++ pages/search.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative z-10 flex flex-col tablet-wide:w-4/5 mx-auto">
     <Breadcrumb />
-    <div class="search_wrapper">
+    <div class="search_wrapper w-4/5 mx-auto">
       <div class="gcse-searchresults-only"></div>
     </div>
   </div>
@@ -27,6 +27,10 @@ export default {
 }
 </script>
 <style scoped lang="postcss">
+.search_wrapper >>> * {
+  @apply p-0;
+}
+
 .search_wrapper >>> .gsc-adBlock,
 .search_wrapper >>> .gsc-positioningWrapper,
 .search_wrapper >>> .gsc-orderby-container,
@@ -35,7 +39,31 @@ export default {
 .search_wrapper >>> .gcsc-more-maybe-branding-root {
   @apply hidden;
 }
-.search_wrapper >>> .gsc-thumbnail-inside a.gs-title {
-  @apply text-blue-main hover:text-blue-main-hover visited:text-blue-main-hover font-lato font-normal text-lg mb-10 transition duration-200;
+.search_wrapper >>> .gsc-webResult.gsc-result .gsc-thumbnail-inside a {
+  @apply inline-block text-blue-main hover:text-blue-main-hover visited:text-blue-main-hover font-lato font-normal text-lg mb-7 transition duration-200;
+}
+
+.search_wrapper >>> .gsc-webResult.gsc-result .gsc-thumbnail-inside a * {
+  @apply text-inherit font-normal font-lato text-lg;
+}
+
+.search_wrapper
+  >>> .gsc-webResult.gsc-result
+  .gsc-thumbnail-inside
+  a:visited
+  * {
+  @apply text-blue-main-hover;
+}
+
+.search_wrapper >>> .gsc-table-result .gs-snippet {
+  @apply text-base font-lato font-normal text-gray-dark;
+}
+
+.search_wrapper >>> .gsc-table-result .gs-snippet b {
+  @apply text-base font-lato font-normal text-gray-dark;
+}
+
+.search_wrapper >>> .gs-webResult.gs-result {
+  @apply py-[50px] border-0 border-b-2 border-solid border-gray-light;
 }
 </style>

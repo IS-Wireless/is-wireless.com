14f257838ef989b7306b7d15f44fb06b8a1c672a
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-06-29 14:42:10 +0200

    AddClass timing issue

--- components/Banner.vue
+++ components/Banner.vue
@@ -154,6 +154,7 @@ export default {
     }
   },
   mounted() {
+    this.$nextTick()
     if (this.data.banner && this.data.banner.length > 1) {
       this.swiperOptionsObject.loop = true
     }
@@ -165,13 +166,14 @@ export default {
     ]
 
     Show(0)
-
+    this.$nextTick()
     function Show(i) {
       if (content[i]) {
         content[i].classList.remove('opacity-0')
       }
 
       setTimeout(() => {
+        this.$nextTick()
         if (i < content.length - 1) {
           i++
           Show(i)

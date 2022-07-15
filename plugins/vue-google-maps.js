44506f1773ca9dfbd8f521385a078dce8f3ffde6
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-07-15 17:40:38 +0200

    Gmap key

--- plugins/vue-google-maps.js
+++ plugins/vue-google-maps.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
+var keyGMAPS = process.env.API_GMAPS
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: process.env.API_GMAPS,
+    key: keyGMAPS,
     language: 'en',
   },
   installComponents: true,

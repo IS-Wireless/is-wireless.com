5b2fb526a53f989c19c4a3ec8a31461b0cc8085b
Kamil <kamil@webo.agency>
2022-08-08 12:00:07 +0200

    modified filter tiles for no filter option

---
+++ pages/4g5gcourses.vue
@@ -0,0 +1,55 @@
+<template>
+  <section_filterable_tiles :data="templateData" />
+</template>
+
+<script>
+import section_filterable_tiles from '../components/filterable-tiles.vue'
+export default {
+  data() {
+    return {
+      templateData: [
+        {
+          link: 'https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-cl4-2022-human-02-01',
+          description:
+            'AI for human empowerment (AI, Data and RoboticsPartnership)',
+          title: 'HORIZON-CL4-2022-HUMAN-02-01',
+        },
+        {
+          link: 'https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-cl4-2022-human-02-01',
+          description:
+            'AI for human empowerment (AI, Data and RoboticsPartnership)',
+          title: 'HORIZON-CL4-2022-HUMAN-02-01',
+        },
+        {
+          link: 'https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-cl4-2022-human-02-01',
+          description:
+            'AI for human empowerment (AI, Data and RoboticsPartnership)',
+          title: 'HORIZON-CL4-2022-HUMAN-02-01',
+        },
+        {
+          description:
+            'AI for human empowerment (AI, Data and RoboticsPartnership)',
+          title: 'HORIZON-CL4-2022-HUMAN-02-01',
+        },
+        {
+          link: '',
+          description:
+            'AI for human empowerment (AI, Data and RoboticsPartnership)',
+          title: 'HORIZON-CL4-2022-HUMAN-02-01',
+        },
+        {
+          link: 'https://ec.europa.eu/info/funding-tenders/opportunities/portal/screen/opportunities/topic-details/horizon-cl4-2022-human-02-01',
+          description:
+            'AI for human empowerment (AI, Data and RoboticsPartnership)',
+          title: 'HORIZON-CL4-2022-HUMAN-02-01',
+        },
+      ],
+    }
+  },
+  components: {
+    section_filterable_tiles,
+  },
+}
+</script>
+
+<style></style>

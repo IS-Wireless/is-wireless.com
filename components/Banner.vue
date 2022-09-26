2d5693cdf8b5f376298464275364b56d70b1defc
Kamil <kamil@webo.agency>
2022-09-26 14:15:42 +0200

    video popup

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,7 +1,6 @@
 <template>
-  <section
-    class="relative w-full h-[480px] phone-wide:h-[640px] tablet-wide:h-[768px]"
-  >
+  <section class="w-full h-[480px] phone-wide:h-[640px] tablet-wide:h-[768px]">
+    <PopupVideo ref="popupVideo" :videoId="'AoI79F43hNQ'" />
     <div
       id="banner"
       class="swiper swiper-container relative w-full h-full bg-white overflow-hidden"
@@ -32,54 +31,84 @@
         v-if="data.default"
         class="absolute inset-0 flex justify-center items-start flex-col text-5xl bg-black/50 z-10 pb-10 tablet:pb-20"
       >
-        <div class="w-4/5 container mx-auto relative">
-          <div class="swiper-pagination"></div>
-          <EffectAppear>
-            <img
-              v-if="data.default.image"
-              ref="content1"
-              width="550"
-              height="160"
-              class="duration-300 mb-10 w-4/5 phone-wide:w-3/5 tablet-wide:w-3/5 desktop:w-1/2 pr-5 tablet:pr-0"
-              :src="data.default.image.url ? data.default.image.url : ''"
-              :alt="data.default.image.alt ? data.default.image.alt : ''"
-              :title="data.default.image.title ? data.default.image.title : ''"
-              loading="eager"
-              :critical="true"
-              preload
-            />
-          </EffectAppear>
-          <EffectAppear :delay="100">
-            <p
-              v-if="data.default && data.default.content"
-              ref="content2"
-              class="duration-300 text-xl tablet:text-2xl text-white mb-10 tablet-wide:mb-[70px] pr-10 tablet:pr-0"
-            >
-              {{ data.default.content }}
-            </p>
-          </EffectAppear>
-          <EffectAppear :delay="200">
-            <div
-              v-if="data.default.links"
-              ref="content3"
-              class="duration-300 flex flex-col tablet:flex-row pr-5 tablet:pr-0"
-            >
-              <CustomLink
-                class="flex w-fit items-center text-sm tablet:text-base text-white hover:text-white font-medium uppercase px-6 tablet:px-8 py-[18px] tablet:py-4 rounded-full tablet:mr-[50px] duration-300 tablet:mb-0 mb-6 after:content-[''] after:block after:w-[25px] after:h-0.5 after:bg-white after:ml-5 after:transition after:duration-300 hover:after:translate-x-1"
-                :class="
-                  index > 0
-                    ? 'border border-solid border-white hover:bg-white/25'
-                    : 'bg-blue-main hover:bg-blue-main-hover'
+        <div
+          class="flex justify-between items-center w-4/5 container mx-auto relative"
+        >
+          <div class="tablet-wide:basis-1/2">
+            <EffectAppear>
+              <img
+                v-if="data.default.image"
+                ref="content1"
+                width="550"
+                height="160"
+                class="duration-300 mb-10 w-4/5 phone-wide:w-3/5 tablet-wide:w-3/5 desktop:w-1/2 pr-5 tablet:pr-0"
+                :src="data.default.image.url ? data.default.image.url : ''"
+                :alt="data.default.image.alt ? data.default.image.alt : ''"
+                :title="
+                  data.default.image.title ? data.default.image.title : ''
                 "
-                v-for="(item, index) in data.default.links"
-                :key="index"
-                :isExternal="false"
-                :url="item.url_link ? item.url_link : ''"
-                :title="item.title_link ? item.title_link : ''"
+                loading="eager"
+                :critical="true"
+                preload
+              />
+            </EffectAppear>
+            <EffectAppear :delay="100">
+              <p
+                v-if="data.default && data.default.content"
+                ref="content2"
+                class="duration-300 text-xl tablet:text-2xl text-white mb-10 tablet-wide:mb-[70px] pr-10 tablet:pr-0"
               >
-              </CustomLink>
+                {{ data.default.content }}
+              </p>
+            </EffectAppear>
+            <EffectAppear :delay="200">
+              <div
+                v-if="data.default.links"
+                ref="content3"
+                class="duration-300 flex flex-col tablet:flex-row pr-5 tablet:pr-0"
+              >
+                <CustomLink
+                  class="flex w-fit items-center text-sm tablet:text-base text-white hover:text-white font-medium uppercase px-6 tablet:px-8 py-[18px] tablet:py-4 rounded-full tablet:mr-[50px] duration-300 tablet:mb-0 mb-6 after:content-[''] after:block after:w-[25px] after:h-0.5 after:bg-white after:ml-5 after:transition after:duration-300 hover:after:translate-x-1"
+                  :class="
+                    index > 0
+                      ? 'border border-solid border-white hover:bg-white/25'
+                      : 'bg-blue-main hover:bg-blue-main-hover'
+                  "
+                  v-for="(item, index) in data.default.links"
+                  :key="index"
+                  :isExternal="false"
+                  :url="item.url_link ? item.url_link : ''"
+                  :title="item.title_link ? item.title_link : ''"
+                >
+                </CustomLink>
+              </div>
+            </EffectAppear>
+          </div>
+          <EffectAppear class="hidden tablet-wide:block" :delay="300">
+            <div class="flex justify-center items-center mr-5">
+              <div
+                class="group cursor-pointer flex justify-center items-center p-2 w-10 tablet:w-20 aspect-square shrink-0 mr-3 border-2 border-white hover:border-blue-main bg-white/20 rounded-full transition duration-300"
+                @click="togglePopup()"
+              >
+                <svg
+                  class="fill-white group-hover:fill-blue-main transition duration-300"
+                  width="28"
+                  height="26"
+                  viewBox="-2 0 21 26"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M21 13L7.65417e-07 25.1244L1.82536e-06 0.875644L21 13Z"
+                  />
+                </svg>
+              </div>
+              <span class="text-white text-xl tablet:text-2xl">
+                PLAY VIDEO
+              </span>
             </div>
           </EffectAppear>
+          <div class="swiper-pagination"></div>
         </div>
       </div>
     </div>
@@ -107,6 +136,7 @@ export default {
   },
   data() {
     return {
+      isPopupOpen: false,
       swiper: null,
       swiperIndex: 0,
       swiperCount: 0,
@@ -155,7 +185,10 @@ export default {
     this.$data.swiper = new Swiper('#banner', this.$data.swiperOptionsObject)
   },
   methods: {
-    picCompute: function (image) {
+    togglePopup() {
+      this.$refs.popupVideo.togglePopup()
+    },
+    picCompute(image) {
       return {
         sources: [
           {
@@ -190,7 +223,7 @@ export default {
 }
 
 .swiper >>> .swiper-pagination.swiper-pagination {
-  @apply w-auto z-20 left-[unset] top-0 right-0 flex flex-col justify-center items-center;
+  @apply w-auto z-20 static flex flex-col justify-center items-center;
 }
 
 .swiper >>> .swiper-pagination .swiper-pagination-bullet {
---
+++ components/popup-video.vue
@@ -0,0 +1,101 @@
+<template>
+  <Transition name="fade" mode="in-out">
+    <div
+      v-if="isOpen"
+      class="fixed inset-0 bg-black/50 z-50 flex justify-center items-center"
+    >
+      <div
+        ref="modal"
+        class="relative bg-white rounded-[5px] w-4/5 aspect-video container"
+        :class="{ 'pointer-events-none': !isOpen }"
+      >
+        <div
+          class="absolute cursor-pointer top-0 right-0 text-black hover:text-gray-dark transition duration-300"
+          @click="togglePopup()"
+        >
+          <svg
+            class="fill-current text-inherit"
+            width="40"
+            height="40"
+            viewBox="7 6 38 38"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              x="17.0625"
+              y="15"
+              width="28.1956"
+              height="2.91678"
+              rx="1"
+              transform="rotate(45 17.0625 15)"
+            />
+            <rect
+              x="15"
+              y="34.9375"
+              width="28.1956"
+              height="2.91679"
+              rx="1"
+              transform="rotate(-45 15 34.9375)"
+            />
+          </svg>
+        </div>
+        <div class="p-10 w-full h-full">
+          <iframe
+            v-if="videoId"
+            class="w-full h-full rounded-[5px]"
+            width="560"
+            height="315"
+            :src="'https://www.youtube.com/embed/' + videoId"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<script>
+import { ref } from '@vue/composition-api'
+import { onClickOutside } from '@vueuse/core'
+export default {
+  name: 'PopupVideo',
+  setup() {
+    const modal = ref(null)
+    let isOpen = ref(false)
+
+    onClickOutside(modal, () => {
+      togglePopup()
+    })
+
+    return { modal, isOpen, togglePopup }
+    function togglePopup() {
+      isOpen.value = !isOpen.value
+    }
+  },
+  head() {
+    return {
+      bodyAttrs: {
+        class: this.isOpen ? 'overflow-hidden' : '',
+      },
+    }
+  },
+  props: {
+    videoId: String,
+  },
+}
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease-in-out;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>

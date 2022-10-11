1a15613f2e04afaa0f13161f26f2d6373ff2f0ae
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-10-11 11:18:17 +0200

    ISW-537 Video

---
+++ components/BannerVideo.vue
@@ -0,0 +1,238 @@
+<template>
+  <section
+    class="relative w-full h-[480px] phone-wide:h-[640px] tablet-wide:h-[768px]"
+  >
+    <PopupVideo
+      v-if="data.default.video.link"
+      ref="popupVideo"
+      :videoSrc="data.default.video.link"
+    />
+    <video-player :options="videoOptions" />
+
+    <div
+      v-if="data.default"
+      class="absolute inset-0 flex justify-center items-start flex-col text-5xl bg-black/50 z-10 pb-10 tablet:pb-20"
+    >
+      <div
+        class="flex justify-between items-center w-4/5 container mx-auto relative"
+      >
+        <div class="tablet-wide:basis-1/2">
+          <EffectAppear>
+            <img
+              v-if="data.default.image"
+              ref="content1"
+              width="550"
+              height="160"
+              class="duration-300 mb-10 w-4/5 phone-wide:w-3/5 tablet-wide:w-3/5 desktop:w-1/2 pr-5 tablet:pr-0"
+              :src="data.default.image.url ? data.default.image.url : ''"
+              :alt="data.default.image.alt ? data.default.image.alt : ''"
+              :title="data.default.image.title ? data.default.image.title : ''"
+              loading="eager"
+              :critical="true"
+              preload
+            />
+          </EffectAppear>
+          <EffectAppear :delay="100">
+            <p
+              v-if="data.default && data.default.content"
+              ref="content2"
+              class="duration-300 text-xl tablet:text-2xl text-white mb-10 tablet-wide:mb-[70px] pr-10 tablet:pr-0"
+            >
+              {{ data.default.content }}
+            </p>
+          </EffectAppear>
+          <EffectAppear :delay="200">
+            <div
+              v-if="data.default.links"
+              ref="content3"
+              class="duration-300 flex flex-col tablet:flex-row pr-5 tablet:pr-0"
+            >
+              <CustomLink
+                class="flex w-fit items-center text-sm tablet:text-base text-white hover:text-white font-medium uppercase px-6 tablet:px-8 py-[18px] tablet:py-4 rounded-full tablet:mr-[50px] duration-300 tablet:mb-0 mb-6 after:content-[''] after:block after:w-[25px] after:h-0.5 after:bg-white after:ml-5 after:transition after:duration-300 hover:after:translate-x-1"
+                :class="
+                  index > 0
+                    ? 'border border-solid border-white hover:bg-white/25'
+                    : 'bg-blue-main hover:bg-blue-main-hover'
+                "
+                v-for="(item, index) in data.default.links"
+                :key="index"
+                :isExternal="false"
+                :url="item.url_link ? item.url_link : ''"
+                :title="item.title_link ? item.title_link : ''"
+              >
+              </CustomLink>
+            </div>
+          </EffectAppear>
+        </div>
+        <EffectAppear
+          v-if="data.default.video.link"
+          class="hidden tablet-wide:block"
+          :delay="300"
+        >
+          <div
+            class="group cursor-pointer flex justify-center items-center mr-5"
+            @click="togglePopup()"
+          >
+            <div
+              class="flex justify-center items-center p-2 w-10 tablet:w-20 aspect-square shrink-0 mr-3 border-2 border-white group-hover:border-blue-main bg-white/20 group-hover:bg-blue-main/20 rounded-full transition duration-300"
+            >
+              <svg
+                class="fill-white group-hover:fill-blue-main transition duration-300"
+                width="28"
+                height="26"
+                viewBox="-2 0 21 26"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M21 13L7.65417e-07 25.1244L1.82536e-06 0.875644L21 13Z"
+                />
+              </svg>
+            </div>
+            <span
+              v-if="data.default.video.title"
+              class="text-white text-xl tablet:text-2xl group-hover:text-blue-main transition duration-300"
+            >
+              {{ data.default.video.title }}
+            </span>
+          </div>
+        </EffectAppear>
+        <div class="swiper-pagination"></div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import EffectAppear from './effect-appear.vue'
+import { Swiper, Pagination, Autoplay } from 'swiper'
+import VideoPlayer from '@/components/VideoPlayer.vue'
+import 'swiper/swiper-bundle.min.css'
+import CustomLink from './custom-link.vue'
+export default {
+  name: 'Banner',
+  components: {
+    CustomLink,
+    EffectAppear,
+    VideoPlayer,
+  },
+  props: {
+    data: {
+      type: Object,
+      required: true,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+      isPopupOpen: false,
+      swiper: null,
+      swiperIndex: 0,
+      swiperCount: 0,
+      swiperOptionsObject: {
+        modules: [Pagination, Autoplay],
+        virtual: false,
+        preventClicksPropagation: false,
+        slidesperview: 1,
+        spaceBetween: 0,
+        direction: 'horizontal',
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: false,
+        },
+        watchOverflow: true,
+        navigation: {
+          nextEl: '[data-slide-next]',
+          prevEl: '[data-slide-prev]',
+        },
+        speed: 500,
+        loopedSlides: 1,
+        loop: this.data.banner
+          ? this.data.banner.length > 1
+            ? true
+            : false
+          : false,
+        keyboard: {
+          enabled: true,
+          onlyInViewport: true,
+        },
+        autoplay: {
+          delay: 6000,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+          clickable: true,
+        },
+      },
+      videoOptions: {
+        autoplay: true,
+        controls: false,
+        muted: true,
+        loop: true,
+        fill: true,
+        sources: [
+          {
+            src: 'https://api.is-wireless.com/wp-content/uploads/2022/10/IS-Wireless-video-background.mp4',
+            type: 'video/mp4',
+          },
+        ],
+      },
+    }
+  },
+  mounted() {
+    this.$data.swiper = new Swiper('#banner', this.$data.swiperOptionsObject)
+  },
+  methods: {
+    togglePopup() {
+      this.$refs.popupVideo.togglePopup()
+    },
+    picCompute(image) {
+      return {
+        sources: [
+          {
+            src: image.url,
+            sizes: {
+              sm: '100vw',
+              md: '100vw',
+              lg: '100vw',
+              xl: '100vw',
+              xxl: '100vw',
+            },
+            media: '(orientation: landscape)',
+          },
+          {
+            src: image.url,
+            width: '1920px',
+            height: '768px',
+            sizes: { default: '100vw', xxs: '100vw', xs: '100vw' },
+          },
+        ],
+        title: image.title,
+        alt: image.alt,
+      }
+    },
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.swiper .swiper-slide picture >>> img {
+  @apply min-w-full h-full object-cover;
+}
+
+.swiper >>> .swiper-pagination.swiper-pagination {
+  @apply hidden w-auto z-20 static pointer-events-none tablet:flex flex-col justify-center items-center;
+}
+
+.swiper >>> .swiper-pagination .swiper-pagination-bullet {
+  @apply w-3 h-3 mx-0 my-[5px]  bg-transparent border border-white border-solid transition duration-200 opacity-100 hover:bg-white/25;
+}
+
+.swiper
+  >>> .swiper-pagination
+  .swiper-pagination-bullet.swiper-pagination-bullet-active {
+  @apply bg-blue-main border-blue-main;
+}
+</style>
---
+++ components/VideoPlayer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="relative w-full h-full">
+    <video
+      ref="videoPlayer"
+      class="video-js object-cover w-full [&>*]:object-cover"
+    ></video>
+  </div>
+</template>
+
+<script>
+import videojs from 'video.js'
+
+import 'video.js/dist/video-js.css'
+export default {
+  name: 'VideoPlayer',
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {}
+      },
+    },
+  },
+  data() {
+    return {
+      player: null,
+    }
+  },
+  mounted() {
+    this.player = videojs(this.$refs.videoPlayer, this.options, () => {
+      this.player.log('onPlayerReady', this)
+    })
+  },
+  beforeDestroy() {
+    if (this.player) {
+      this.player.dispose()
+    }
+  },
+}
+</script>

e89c549580c2a88aef59c7b787857e060f7ae28d
Kamil <kamil@webo.agency>
2022-11-23 15:12:54 +0100

    block form changed to 2col section

--- components/block-form.vue
+++ components/block-form.vue
@@ -1,11 +1,15 @@
 <template>
-    <div class="flex flex-col tablet:flex-row">
+<div class="text-inherit relative z-10 flex flex-col tablet-wide:flex-row w-full">
+    <div class="tablet-wide:flex-[1_1_50%] tablet-wide:w-1/2 tablet-wide:mr-8">
+        <section_content :data="data"/>
+    </div>
+    <div class="tablet-wide:flex-[1_1_50%] tablet-wide:w-1/2 tablet-wide:ml-8 flex flex-col tablet:flex-row">
       <div v-if="data"
-        class="max-w-[820px] text-gray-darker mb-20 tablet:mb-0 basis-2/3 desktopWide:basis-auto desktopWide:shrink-0"
+        class="max-w-[820px] text-gray-darker mb-20 tablet:mb-10 tablet:mt-20 desktopWide:shrink-0"
       >
         <form name="contactForm" method="post" enctype="multipart/form-data" class="flex flex-col">
-          <ul class="flex flex-wrap gap-5 justify-between mb-4">
-            <li v-for="item,index in data.inputs" :key="index" :class="[item.fullWidth ? 'basis-full':'basis-[calc(50%_-_10px)]']">
+          <ul class="flex flex-wrap gap-2.5 tablet:gap-5 justify-between mb-4">
+            <li v-for="item,index in data.inputs" :key="index" :class="[item.fullWidth ? 'basis-full':'basis-full tablet:basis-[calc(50%_-_10px)]']">
                 <FormField :data="item" />
             </li>
             <li class="w-full flex items-start px-2.5" v-if="data.privacyText">
@@ -27,67 +31,67 @@
         </div>
       </div>
     </div>
+</div>
+
+
   </template>
   
   <script>
+import section_content from './content-static.vue';
+
   export default {
-    name: "section_form",
+    name: "section_two_column_contact",
     props: {
-        data:Object
+        data: Object
     },
     methods: {
-      isEmailValid(email) {
-        return String(email)
-          .toLowerCase()
-          .match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          )
-          ? true
-          : false;
-      },
-      isPhoneValid(phone) {
-        return String(phone)
-          .match(/^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/)
-          ? true
-          : false;
-      },
-      checkRequired(form){
-        const formData = new FormData(form);
-        const requiredInputs = form.querySelectorAll('[required="required"]')
-        let validityArray = []
-        for (const input of requiredInputs.values()) {
-            validityArray.push(input.checkValidity());
-        }
-        if (formData.get('tel')) {
-            validityArray.push(this.isPhoneValid(formData.get('tel')))
-        }
-        return validityArray.every(el => el === true)
-        
-      },
-      sendForm() {
-        const formContainer = document.forms.contactForm
-        const formData = new FormData(formContainer);
-        if (formData.get('mail') && this.checkRequired(formContainer) && this.isEmailValid(formData.get('mail'))) {
-        
-            console.log(formData);
-
-        //   this.formRequest(formData).then( (result) => {
-        //       console.log(result)
-        //   }).catch( (error) => {
-        //       console.error('Contact form could not be send', error)
-        //   });
-        }else{
-            console.log('Fulfill required fields correctly');
-        }
-        
-      },
-      async formRequest(data) {
-       return await $fetch( '/api/form-send', 
-        { 
-          method: 'POST',
-          body: data
-        });
-      },
+        isEmailValid(email) {
+            return String(email)
+                .toLowerCase()
+                .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+                ? true
+                : false;
+        },
+        isPhoneValid(phone) {
+            return String(phone)
+                .match(/^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/)
+                ? true
+                : false;
+        },
+        checkRequired(form) {
+            const formData = new FormData(form);
+            const requiredInputs = form.querySelectorAll("[required=\"required\"]");
+            let validityArray = [];
+            for (const input of requiredInputs.values()) {
+                validityArray.push(input.checkValidity());
+            }
+            if (formData.get("tel")) {
+                validityArray.push(this.isPhoneValid(formData.get("tel")));
+            }
+            return validityArray.every(el => el === true);
+        },
+        sendForm() {
+            const formContainer = document.forms.contactForm;
+            const formData = new FormData(formContainer);
+            if (formData.get("mail") && this.checkRequired(formContainer) && this.isEmailValid(formData.get("mail"))) {
+                console.log(formData);
+                //   this.formRequest(formData).then( (result) => {
+                //       console.log(result)
+                //   }).catch( (error) => {
+                //       console.error('Contact form could not be send', error)
+                //   });
+            }
+            else {
+                console.log("Fulfill required fields correctly");
+            }
+        },
+        async formRequest(data) {
+            return await $fetch("/api/form-send", {
+                method: "POST",
+                body: data
+            });
+        },
     },
-  };
+    components: { section_content }
+};
   </script>
--- pages/_.vue
+++ pages/_.vue
@@ -120,7 +120,7 @@ export default {
     section_content_columns_center_image: () =>
       import('~/components/content-section-center.vue'),
     section_person_list: () => import('~/components/rnd-team.vue'),
-    section_form: () => import('~/components/block-form.vue'),
+    section_two_column_contact: () => import('~/components/block-form.vue'),
   },
   async asyncData({ route, payload, store, $config }) {
     if (

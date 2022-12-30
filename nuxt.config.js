8c5a636dbbe93880aa53509ad79916ccb93aa56f
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-12-30 10:01:09 +0100

    ISW-568 Check spa

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,8 +29,8 @@ export default {
     },
   },
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'universal',
-  ssr: false,
+  target: 'static',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'IS-Wireless #5GMadeTogether',

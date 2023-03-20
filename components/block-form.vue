5b41a2575caddc2d37a4dca3ccadea0a4f5202c2
Triloworld <2671726+Triloworld@users.noreply.github.com>
2023-03-20 15:03:37 +0100

    Contact form

--- components/block-form.vue
+++ components/block-form.vue
@@ -1,23 +1,49 @@
 <template>
-<div class="text-inherit relative z-10 flex flex-col tablet-wide:flex-row w-full">
+  <div
+    class="text-inherit relative z-10 flex flex-col tablet-wide:flex-row w-full"
+  >
     <div class="tablet-wide:flex-[1_1_50%] tablet-wide:w-1/2 tablet-wide:mr-8">
-        <section_content :data="data"/>
+      <section_content :data="data" />
     </div>
-    <div class="tablet-wide:flex-[1_1_50%] tablet-wide:w-1/2 tablet-wide:ml-8 flex flex-col tablet:flex-row">
-      <div v-if="data"
+    <div
+      class="tablet-wide:flex-[1_1_50%] tablet-wide:w-1/2 tablet-wide:ml-8 flex flex-col tablet:flex-row"
+    >
+      <div
+        v-if="data"
         class="max-w-[820px] text-gray-darker mb-20 tablet:mb-10 tablet:mt-20 desktopWide:shrink-0"
       >
-        <form name="contactForm" method="post" enctype="multipart/form-data" class="flex flex-col">
+        <form
+          name="contactForm"
+          method="post"
+          enctype="multipart/form-data"
+          class="flex flex-col"
+        >
           <ul class="flex flex-wrap gap-2.5 tablet:gap-5 justify-between mb-4">
-            <li v-for="item,index in data.inputs" :key="index" :class="[item.fullWidth ? 'basis-full':'basis-full tablet:basis-[calc(50%_-_10px)]']">
-                <FormField :data="item" />
+            <li
+              v-for="(item, index) in data.inputs"
+              :key="index"
+              :class="[
+                item.fullWidth
+                  ? 'basis-full'
+                  : 'basis-full tablet:basis-[calc(50%_-_10px)]',
+              ]"
+            >
+              <FormField :data="item" />
             </li>
             <li class="w-full flex items-start px-2.5" v-if="data.privacyText">
-                <input type="checkbox" name="acceptance" id="acceptance" required="required" class="shrink-0 mx-2.5 mt-1">
-                <label for="acceptance"
-                  class="block text-xs desktop:text-sm text-gray-dark"
-                    v-html="data.privacyText">
-                </label>
+              <input
+                type="checkbox"
+                name="acceptance"
+                id="acceptance"
+                required="required"
+                class="shrink-0 mx-2.5 mt-1"
+              />
+              <label
+                for="acceptance"
+                class="block text-xs desktop:text-sm text-gray-dark"
+                v-html="data.privacyText"
+              >
+              </label>
             </li>
           </ul>
         </form>
@@ -26,72 +52,75 @@
             class="text-sm ml-auto w-auto block text-white uppercase px-10 py-3 rounded-full bg-blue-main hover:bg-gray-light hover:text-black duration-300 mt-3 mb-4 tablet:mb-3"
             @click="sendForm()"
             v-html="data.buttonText ? data.buttonText : 'Send'"
-          >
-          </button>
+          ></button>
         </div>
       </div>
     </div>
-</div>
+  </div>
+</template>
 
+<script>
+import section_content from './content-static.vue'
 
-  </template>
-  
-  <script>
-import section_content from './content-static.vue';
-
-  export default {
-    name: "section_two_column_contact",
-    props: {
-        data: Object
+export default {
+  name: 'section_two_column_contact',
+  props: {
+    data: Object,
+  },
+  methods: {
+    isEmailValid(email) {
+      return String(email)
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+        ? true
+        : false
     },
-    methods: {
-        isEmailValid(email) {
-            return String(email)
-                .toLowerCase()
-                .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
-                ? true
-                : false;
-        },
-        isPhoneValid(phone) {
-            return String(phone)
-                .match(/^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/)
-                ? true
-                : false;
-        },
-        checkRequired(form) {
-            const formData = new FormData(form);
-            const requiredInputs = form.querySelectorAll("[required=\"required\"]");
-            let validityArray = [];
-            for (const input of requiredInputs.values()) {
-                validityArray.push(input.checkValidity());
-            }
-            if (formData.get("tel")) {
-                validityArray.push(this.isPhoneValid(formData.get("tel")));
-            }
-            return validityArray.every(el => el === true);
-        },
-        sendForm() {
-            const formContainer = document.forms.contactForm;
-            const formData = new FormData(formContainer);
-            if (formData.get("mail") && this.checkRequired(formContainer) && this.isEmailValid(formData.get("mail"))) {
-                console.log(formData);
-                //   this.formRequest(formData).then( (result) => {
-                //       console.log(result)
-                //   }).catch( (error) => {
-                //       console.error('Contact form could not be send', error)
-                //   });
-            }
-            else {
-                console.log("Fulfill required fields correctly");
-            }
-        },
-        async formRequest(data) {
-            return await $fetch("/api/form-send", {
-                method: "POST",
-                body: data
-            });
-        },
+    isPhoneValid(phone) {
+      return String(phone).match(
+        /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
+      )
+        ? true
+        : false
     },
-    components: { section_content }
-};
-  </script>
+    checkRequired(form) {
+      const formData = new FormData(form)
+      const requiredInputs = form.querySelectorAll('[required="required"]')
+      let validityArray = []
+      for (const input of requiredInputs.values()) {
+        validityArray.push(input.checkValidity())
+      }
+      if (formData.get('tel')) {
+        validityArray.push(this.isPhoneValid(formData.get('tel')))
+      }
+      return validityArray.every((el) => el === true)
+    },
+    sendForm() {
+      const formContainer = document.forms.contactForm
+      const formData = new FormData(formContainer)
+      if (
+        formData.get('mail') &&
+        this.checkRequired(formContainer) &&
+        this.isEmailValid(formData.get('mail'))
+      ) {
+        console.log(formData)
+        //   this.formRequest(formData).then( (result) => {
+        //       console.log(result)
+        //   }).catch( (error) => {
+        //       console.error('Contact form could not be send', error)
+        //   });
+      } else {
+        console.log('Fulfill required fields correctly')
+      }
+    },
+    async formRequest(data) {
+      return await $fetch('/api/contact', {
+        method: 'POST',
+        body: data,
+      })
+    },
+  },
+  components: { section_content },
+}
+</script>
---
+++ functions/api/contact.js
@@ -0,0 +1,296 @@
+addEventListener('fetch', (event) => {
+  const request = event.request
+  if (request.method === 'OPTIONS') {
+    event.respondWith(handleOptions(request))
+  } else {
+    event.respondWith(handle(request))
+  }
+})
+
+// Helper function to return JSON response
+const JSONResponse = (message, status = 200) => {
+  let headers = {
+    headers: {
+      'content-type': 'application/json;charset=UTF-8',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+
+    status: status,
+  }
+
+  let response = {
+    message: message,
+  }
+
+  return new Response(JSON.stringify(response), headers)
+}
+
+const urlfy = (obj) =>
+  Object.keys(obj)
+    .map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(obj[k]))
+    .join('&')
+
+function handleOptions(request) {
+  if (
+    request.headers.get('Origin') !== null &&
+    request.headers.get('Access-Control-Request-Method') !== null &&
+    request.headers.get('Access-Control-Request-Headers') !== null
+  ) {
+    // Handle CORS pre-flight request.
+    return new Response(null, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Client-Key',
+      },
+    })
+  } else {
+    // Handle standard OPTIONS request.
+    return new Response(null, {
+      headers: {
+        Allow: 'GET, HEAD, POST, OPTIONS',
+      },
+    })
+  }
+}
+
+// function authorize(request) {
+//     if (request.headers.get("Client-Key") !== config.client_key) {
+//         throw new Error('Unauthorized request')
+//     }
+// }
+
+// Utility function to validate form fields
+function validateInput(form, config) {
+  // if (form[config.honeypot_field] !== "") {
+  //     throw new Error('Invalid request')
+  // }
+
+  // Validate form inputs
+  for (let i = 0; i < config.form_fields.length; i++) {
+    let field = config.form_fields[i]
+    if (form[field] === '') {
+      throw new Error(`${field} is required`)
+    }
+  }
+
+  // Validate email field
+  let email_regex =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  if (
+    form[config.email_field] == '' ||
+    !email_regex.test(form[config.email_field])
+  ) {
+    console.log(
+      form[config.email_field] == '' ||
+        !email_regex.test(form[config.email_field])
+    )
+    throw new Error('Please, enter valid email address')
+  }
+  if (
+    form[config.admin_email] == '' ||
+    !email_regex.test(form[config.admin_email])
+  ) {
+    throw new Error(
+      'No admin email address found. Please contact site administrator'
+    )
+  }
+}
+
+function generateAdminOptions(form, config, env) {
+  let admin_data = {
+    from: config.from,
+    to: config.admin_email,
+    subject: `${form.company}: New message from ${form.name}`,
+    text: JSON.stringify(form),
+    template: 'hello',
+    'h:X-Mailgun-Variables': JSON.stringify({
+      // be sure to stringify your payload
+      name: form.name,
+      company: form.company,
+      phone: form.phone,
+      description: form.description,
+      service: form.service,
+    }),
+    'o:tag': 'www',
+    'h:Reply-To': form.mail,
+    // attachments: [{
+    //     filename: 'mailgun.png',
+    //     data: Buffer.from(form.attachment, 'binary').toString('base64')
+    //   }],
+  }
+
+  let admin_options = {
+    method: 'POST',
+    headers: {
+      Authorization:
+        'Basic ' +
+        btoa(
+          'api:' + env.MAILGUN_API_KEY || 'key-398c9b563c4855e62deb4d9cd020ddc8'
+        ),
+      'Content-Type': 'application/x-www-form-urlencoded',
+      //"Content-Length": JSON.stringify(admin_data).length
+    },
+    inline: form.attachment,
+    body: urlfy(admin_data),
+  }
+
+  return admin_options
+}
+
+function generateUserOptions(form, config, env) {
+  let user_data = {
+    from: config.from,
+    to: form.mail,
+    subject: `Dziękujemy za kontakt z webo.agency ${form.name}`,
+    text: JSON.stringify(form),
+    template: 'hello',
+    'h:X-Mailgun-Variables': JSON.stringify({
+      // be sure to stringify your payload
+      name: form.name,
+      company: form.company,
+      phone: form.phone,
+      description: form.description,
+      service: form.service,
+    }),
+    'o:tag': 'www',
+    'h:Reply-To': config.admin_email, // reply to admin
+    // attachments: [{
+    //     filename: 'mailgun.png',
+    //     data: Buffer.from(form.attachment, 'binary').toString('base64')
+    //   }]
+  }
+
+  let user_options = {
+    method: 'POST',
+    headers: {
+      Authorization:
+        'Basic ' +
+        btoa(
+          'api:' + env.MAILGUN_API_KEY || 'key-398c9b563c4855e62deb4d9cd020ddc8'
+        ),
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: urlfy(user_data),
+  }
+
+  return user_options
+}
+
+export async function onRequestPost(context) {
+  const {
+    request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+    waitUntil, // same as ctx.waitUntil in existing Worker API
+    next, // used for middleware or to fetch assets
+    data, // arbitrary space for passing data between middlewares
+  } = context
+
+  const formData = await request.formData()
+
+  const form = JSON.parse(JSON.stringify(Object.fromEntries(formData)))
+
+  // Script configuration
+  const config = {
+    client_key: context.env.CLIENT_API_KEY,
+    mailgun_key:
+      context.env.MAILGUN_API_KEY || 'key-398c9b563c4855e62deb4d9cd020ddc8',
+    mailgun_domain:
+      context.env.MAILGUN_API_DOMAIN ||
+      'sandbox59a0930a84614b27b3e712756266a274.mailgun.org',
+    from:
+      formData.mail ||
+      context.env.EMAIL_FROM ||
+      'mailgun@sandbox59a0930a84614b27b3e712756266a274.mailgun.org',
+    email_field: 'mail', // email field name
+    admin_email: context.env.EMAIL_TO || 'test@webo.agency',
+    form_fields: ['name', 'description', 'company', 'service'], // list of required fields
+    honeypot_field: 'email2', // honeypot field name
+  }
+
+  const messgage = {
+    from: `${formData.name} <${
+      formData.mail ||
+      context.env.EMAIL_FROM ||
+      'mailgun@sandbox59a0930a84614b27b3e712756266a274.mailgun.org'
+    }>`,
+    to: [context.env.EMAIL_TO || 'test@webo.agency'],
+    subject: `${formData.company}`,
+    text: JSON.stringify(formData),
+    template: 'hello',
+    'h:X-Mailgun-Variables': JSON.stringify({
+      // be sure to stringify your payload
+      name: formData.name,
+      company: formData.company,
+      phone: formData.phone,
+      description: formData.description,
+    }),
+    'o:tag': 'www',
+    'h:Reply-To': formData.mail,
+  }
+
+  // console.log(request)
+  // Authenticate pre-distributed API key
+  // try {
+  //     authorize(request);
+  // } catch (err) {
+  //     return JSONResponse("Unauthorized request", 401);
+  // }
+
+  // Validate form fields
+
+  // try {
+  //     validateInput(form, config);
+  // } catch (err)  {
+  //     return JSONResponse(err.message, 400);
+  // }
+
+  // const user_options = generateUserOptions(form, config);
+
+  // Construct admin and user email and options
+
+  console.log(config)
+  try {
+    const admin_options = generateAdminOptions(form, config, env)
+    const user_options = generateUserOptions(form, config, env)
+    // Config to submit each email
+    const options = [admin_options, user_options]
+
+    // Send admin and user emails
+    try {
+      let data = await Promise.all(
+        options.map((opt) =>
+          fetch(
+            `${context.env.MAILGUN_ENDPOINT || 'https://api.mailgun.net'}/v3/${
+              context.env.MAILGUN_API_DOMAIN ||
+              'sandbox59a0930a84614b27b3e712756266a274.mailgun.org'
+            }/messages`,
+            opt
+          )
+            .then((response) => {
+              if (response.status != 200) {
+                throw new Error('Email failed to send')
+              }
+            })
+            .catch((err) => {
+              throw new Error('Email failed to send')
+            })
+        )
+      )
+      return JSONResponse('Message has been sent')
+    } catch (err) {
+      return JSONResponse(
+        'Failed to send email, please contact website administrator',
+        500
+      )
+    }
+  } catch (err) {
+    return JSONResponse(
+      'Failed to generate email, please contact website administrator',
+      500
+    )
+  }
+}

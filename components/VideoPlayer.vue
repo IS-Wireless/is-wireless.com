215d02656c292ddd3df22208f972acabb9ee008b
Kamil <kamil@webo.agency>
2023-04-27 12:52:09 +0200

    noscript fixed, video player img placeholder added

--- components/VideoPlayer.vue
+++ components/VideoPlayer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative w-full h-full">
-    <!-- <video
+    <video
       v-if="isMounted"
       playsinline
       loop
@@ -11,8 +11,22 @@
       class="block align-top bg-black object-cover w-full [&>*]:w-full [&>*]:h-full [&>*]:object-none desktop:[&>*]:object-cover h-full"
     ></video>
 
-    <nuxt-picture
-      src="video_ poster.jpg"
+    <component v-if="$isServer" :is="'noscript'">
+      <video
+      autoplay
+      playsinline
+      loop
+      muted
+      loading="lazy"
+      preload="none"
+      class="block align-top bg-black object-cover w-full [&>*]:w-full [&>*]:h-full [&>*]:object-none desktop:[&>*]:object-cover h-full"
+    >
+        <source v-for="source,index in options.sources" :key="index" :src="source.src" :type="source.type" >
+      </video>
+    </component>
+
+    <nuxt-picture v-if="placeholderImg"
+      :src="placeholderImg"
       alt="Poster"
       width="1920px"
       height="768px"
@@ -22,7 +36,7 @@
       preload
       critical="true"
       ref="videoPlayerPoster"
-    /> -->
+    />
   </div>
 </template>
 
@@ -32,6 +46,9 @@ import videojs from 'video.js/core.es.js'
 export default {
   name: 'VideoPlayer',
   props: {
+    placeholderImg: {
+      type: String
+    },
     options: {
       type: Object,
       default() {
@@ -47,17 +64,19 @@ export default {
   },
   mounted() {
     this.isMounted = true
-    // this.$nextTick(() => {
-    //   this.player = videojs(
-    //     this.$refs.videoPlayer,
-    //     this.options,
-    //     this.playerReady
-    //   ).play()
-    // })
+    this.$nextTick(() => {
+      this.player = videojs(
+        this.$refs.videoPlayer,
+        this.options,
+        this.playerReady
+      ).play()
+    })
   },
   methods: {
     playerReady() {
-      this.$refs['videoPlayerPoster'].$el.classList.add('invisible')
+      if (this.placeholderImg) {
+        this.$refs['videoPlayerPoster'].$el.classList.add('invisible')
+      }
     },
   },
   beforeDestroy() {

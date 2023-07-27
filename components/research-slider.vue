063699ec54ac6d144cb3ea3dcd87840001b45e08
Kamil <kamil@webo.agency>
2023-07-27 16:06:45 +0200

    section research slider base

---
+++ components/research-slider.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="w-full">
+        <div class="flex flex-col tablet:flex-row gap-5">
+            <SectionHeader v-if="data.title" :title="data.title" class="text-gray-darkest [&_h2]:!mt-0"/>
+            <div v-if="data.content" v-html="data.content" class="text-gray-dark text-xl tablet:text-2xl">
+            </div>
+        </div>
+      <div
+        v-for="(item, index) in data.selected"
+        :key="index"
+        :data="subpagesData[item.ID]"
+      ></div>
+    </div>
+  </template>
+  
+  <script>
+  import CustomLink from './custom-link.vue'
+  
+  export default {
+    name: 'section_research_slider',
+    components: {
+      CustomLink,
+    },
+    props: {
+      data: {
+        type: Object,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        subpagesData: this.$store.getters['general/getPagesData'],
+      }
+    },
+  }
+  </script>
+  
+  <style lang="postcss" scoped>
+  </style>
+  

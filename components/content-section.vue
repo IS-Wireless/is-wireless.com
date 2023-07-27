4ebef4d49979f440ac626f38b8ff768ca780e31f
Kamil <kamil@webo.agency>
2023-07-27 09:59:09 +0200

    banner with description, section_two_column updates

--- components/content-section.vue
+++ components/content-section.vue
@@ -5,17 +5,27 @@
     >
       <div
         class="tablet-wide:flex-[1_1_50%] tablet-wide:w-1/2 tablet-wide:mr-8"
+        :class="{'tablet:py-10 tablet-wide:py-[100px]':fullImg || data.fullImg}"
       >
         <SectionHeader v-if="data.title" :title="data.title" />
-        <div
-          v-if="data.content"
-          class="content-html text-inherit"
-          v-html="data.content"
-        ></div>
-        <List v-if="data.list" :data="data.list" />
+        <div :class="{'tablet:p-[50px]':fullImg || data.fullImg}">
+          <div
+            v-if="data.content"
+            class="content-html text-inherit"
+            v-html="data.content"
+          ></div>
+          <CustomLink
+            v-if="data.button_url && data.button_text"
+            class="flex items-center w-fit text-sm tablet:text-base text-white hover:text-white font-medium uppercase mt-5 tablet:mt-11 px-6 tablet:px-8 py-4 rounded-full duration-300 after:content-[''] after:block after:w-[25px] after:h-0.5 after:bg-white after:ml-5 after:transition after:duration-300 hover:after:translate-x-1 bg-blue-main hover:bg-blue-main-hover"
+            :url="data.button_url"
+            :title="data.button_text"
+          />
+          <List v-if="data.list" :data="data.list" />
+        </div>
       </div>
       <div
-        class="tablet-wide:flex-[1_1_50%] tablet-wide:w-1/2 tablet-wide:ml-8"
+        class="tablet-wide:flex-[1_1_50%] tablet-wide:w-1/2"
+        :class="{'tablet-wide:ml-8' :!fullImg || data.fullImg}"
       >
         <Globe v-if="globeImg" />
         <CustomLink
@@ -24,20 +34,22 @@
           :isExternal="true"
         >
           <component
-            :is="fullImg ? 'HomepageImage' : 'SectionImage'"
+            :is="fullImg || data.fullImg ? 'HomepageImage' : 'SectionImage'"
             :url="data.image.url ? data.image.url : ''"
             :title="data.image.title ? data.image.title : ''"
             :alt="data.image.alt ? data.image.alt : ''"
             :imageDecoration="data.image_decoration"
+            :class="{'!pl-0 mx-[-12.5%] tablet-wide:mx-0 tablet-wide:absolute inset-y-0 left-1/2 right-[-12.5%]':fullImg || data.fullImg}"
           />
         </CustomLink>
         <component
           v-else-if="data.image"
-          :is="fullImg ? 'HomepageImage' : 'SectionImage'"
+          :is="fullImg || data.fullImg ? 'HomepageImage' : 'SectionImage'"
           :url="data.image.url ? data.image.url : ''"
           :title="data.image.title ? data.image.title : ''"
           :alt="data.image.alt ? data.image.alt : ''"
           :imageDecoration="data.image_decoration"
+          :class="{'!pl-0 mx-[-12.5%] tablet-wide:mx-0 tablet-wide:absolute inset-y-0 left-1/2 right-[-12.5%]':fullImg || data.fullImg}"
         />
       </div>
     </div>
@@ -85,9 +97,10 @@ export default {
   computed: {
     mergeClass: function () {
       return (
-        'bg-white relative w-full pb-8 tablet-wide:pb-14 text-gray-dark pt-10 tablet-wide:pt-16' +
+        'bg-white relative w-full text-gray-dark' +
         ' ' +
-        this.staticClass
+        this.staticClass + ' ' +
+        + this.fullImg || data.fullImg ? ' ' : 'pb-8 tablet-wide:pb-14 pt-10 tablet-wide:pt-16 '
       )
     },
   },
---
+++ components/static-banner-description.vue
@@ -0,0 +1,54 @@
+<template>
+  <div
+  ref="baner_container"
+    class="w-full min-h-[500px] tablet-wide:min-h-[700px] bg-cover bg-center pt-20 pb-10 tablet:pt-[200px] tablet:pb-[100px]"
+    :style="{ backgroundImage: 'url(' + backgroundUrl + ')' }"
+  >
+    <div class="h-full w-4/5 container mx-auto flex flex-col items-center">
+      <h1
+        v-if="title"
+        class="text-[40px] tablet:text-[80px] text-white mb-5 tablet:mb-12 text-center max-w-5xl leading-tight"
+        v-html="title"
+      ></h1>
+      <div class="w-[124px] h-[5px] bg-blue-main mb-10 tablet:mb-20"></div>
+      <p v-if="description" class="max-w-3xl mb-10 tablet:mb-20 text-xl tablet:text-2xl text-center text-white" v-html="description"></p>
+      <button class="block w-fit p-1 group cursor-pointer" @click="scrollDown()">
+        <svg width="34" height="54" viewBox="0 0 34 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path class="group-hover:translate-y-1 transition duration-300" d="M17 40L11 34M17 40L23 34M17 40V29.5M17 24V26.5" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+          <rect class="stroke-white group-hover:stroke-blue-main transition duration-300" x="1" y="1" width="32" height="52" rx="16" stroke-width="2"/>
+        </svg>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StaticBannerDescription',
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    description: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    backgroundUrl: {
+      type: String,
+      required: true,
+      default: '',
+    },
+  },
+  methods:{
+    scrollDown(){
+      const offset = this.$refs['baner_container'].clientHeight
+      window.scrollTo(0,offset)
+    }
+  }
+}
+</script>
+
+<style></style>

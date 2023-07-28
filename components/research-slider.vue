8f18767ef5b351db882caf45e476196d88ab89cf
Kamil <kamil@webo.agency>
2023-07-28 08:37:36 +0200

    research logos swiper

--- components/research-slider.vue
+++ components/research-slider.vue
@@ -1,20 +1,48 @@
 <template>
     <div class="w-full">
         <div class="flex flex-col tablet:flex-row gap-5">
-            <SectionHeader v-if="data.title" :title="data.title" class="text-gray-darkest [&_h2]:!mt-0"/>
-            <div v-if="data.content" v-html="data.content" class="text-gray-dark text-xl tablet:text-2xl">
+            <SectionHeader v-if="data.title" :title="data.title" class="text-gray-darkest [&_h2]:!mt-0 max-w-[420px] shrink-0"/>
+            <div v-if="data.content" v-html="data.content" class="text-gray-dark [&_p]:text-xl tablet:[&_p]:text-2xl mb-5">
             </div>
         </div>
-      <div
-        v-for="(item, index) in data.selected"
-        :key="index"
-        :data="subpagesData[item.ID]"
-      ></div>
+      <div id="researchSwiper" class="swiper swiper-container overflow-hidden h-14 tablet:h-[70px]">
+          <div
+            class="swiper-wrapper select-none flex w-full h-full items-center"
+          >
+            <div
+              v-for="(item, index) in selectedPages"
+              :key="index"
+              class="swiper-slide swiper-duplicate-load-fix h-full pr-10 basis-full phone:basis-1/2 tablet-small:basis-1/3 desktop:basis-1/5 flex-grow-0 flex-shrink-0"
+            >
+            <CustomLink :url="item.link">
+              <nuxt-picture
+                v-if="item.acf && item.acf.page_icon"
+                fit="outside"
+                :src="item.acf.page_icon.replace(
+                        'www.is-wireless.com',
+                        'api.is-wireless.com'
+                      )
+                "
+                class="h-full flex justify-center"
+                :title="item.acf.page_title ? item.acf.page_title : ''"
+                sizes="sm:180px lg:360px"
+                :img-attrs="{
+                  loading: 'lazy',
+                  class:
+                    'w-auto h-full object-contain custom-filter duration-300 max-w-[200px] m-0 opacity-0 transition w-full',
+                }"
+                @load="imageAnimateLoad($event)"
+              />
+            </CustomLink>
+            </div>
+          </div>
+        </div>
     </div>
   </template>
   
   <script>
   import CustomLink from './custom-link.vue'
+  import { Swiper, Navigation, Autoplay } from 'swiper'
   
   export default {
     name: 'section_research_slider',
@@ -30,11 +58,66 @@
     data() {
       return {
         subpagesData: this.$store.getters['general/getPagesData'],
+        swiper: null,
+        swiperOptionsObject: {
+        modules: [Navigation, Autoplay],
+        slidesPerView: 'auto',
+        spaceBetween: 0,
+        direction: 'horizontal',
+        speed: 600,
+        loop: true,
+        loopedSlides: 5,
+        autoplay: {
+          delay: 3000,
+          pauseOnMouseEnter: true,
+          disableOnInteraction: false,
+        },
+        breakpoints:{
+          1248:{
+            loopedSlides: 3,
+          }
+        }
+      },
       }
     },
+    computed:{
+      selectedPages(){
+        let pages = []
+        let selectedIds = this.data.selected
+        if (selectedIds) {
+          for (let i = 0; i < selectedIds.length; i++) {
+            let page = this.subpagesData[selectedIds[i].ID]
+            if (page?.acf?.page_icon) {
+              pages.push(page)
+            }
+          }
+        }
+
+        return pages 
+      }
+    },
+    mounted(){
+      this.swiper = new Swiper('#researchSwiper',this.swiperOptionsObject)
+    },
+    methods: {
+    imageAnimateLoad(e) {
+      e.currentTarget.classList.remove('opacity-0')
+    },
+  },
   }
   </script>
   
   <style lang="postcss" scoped>
+  >>> .custom-filter {
+    filter: saturate(0) opacity(0.5);
+  }
+  >>> .custom-filter:hover {
+    filter: saturate(1) opacity(1);
+  }
+  
+  .swiper-duplicate-load-fix /deep/ .opacity-0[loading='eager'],
+  .swiper-duplicate-load-fix.swiper-slide-duplicate /deep/ .opacity-0 {
+    opacity: 1;
+  }
   </style>
   

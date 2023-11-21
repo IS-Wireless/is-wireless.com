a06a042a6becc22b09c24050df7ac7ca780547ac
Kamil <kamil@webo.agency>
2023-11-21 13:36:15 +0100

    endpoint GET method , routeRules added

--- nuxt.config.js
+++ nuxt.config.js
@@ -153,6 +153,7 @@ export default defineNuxtConfig({
   // nitro: {
   //   compressPublicAssets: true,
   // },
+  routeRules: { '/api/contact': { prerender: true }},
 
   image: {
     densities: [1, 2],

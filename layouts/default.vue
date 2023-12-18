4dabc18797b788af88b870fba80a1dbd1bff4b2b
Triloworld <2671726+Triloworld@users.noreply.github.com>
2023-12-18 18:31:47 +0100

    Transition fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,9 @@
       :side-menu="sideMenuData"
       :socials="socialMenuData"
     />
-    <slot></slot>
+    <div>
+      <slot />
+    </div>
     <ScrollToTop />
     <!-- <PopupCookies /> -->
     <Footer
@@ -23,130 +25,130 @@
 </template>
 
 <script>
-import { useGeneralStore } from '~/store/general'
+import { useGeneralStore } from "~/store/general";
 export default {
-  name: 'Default',
+  name: "Default",
   computed: {
     topMenuData() {
-      let menu = []
-      let data = useGeneralStore().data
-      if (data['menu_locations'] && data['menu_locations']['main_navigation']) {
-        let topMenuID = data['menu_locations']['main_navigation']['ID']
+      let menu = [];
+      let data = useGeneralStore().data;
+      if (data["menu_locations"] && data["menu_locations"]["main_navigation"]) {
+        let topMenuID = data["menu_locations"]["main_navigation"]["ID"];
 
-        if (data['menu'][topMenuID]['items']) {
-          menu = data['menu'][topMenuID]['items']
+        if (data["menu"][topMenuID]["items"]) {
+          menu = data["menu"][topMenuID]["items"];
         }
       }
-      return menu
+      return menu;
     },
 
     sideMenuData() {
-      let menu = []
-      let data = useGeneralStore().data
+      let menu = [];
+      let data = useGeneralStore().data;
 
-      if (data['menu_locations'] && data['menu_locations']['sidebar']) {
-        let sideMenuID = data['menu_locations']['sidebar']['ID']
-        menu = data['menu'][sideMenuID]['items']
+      if (data["menu_locations"] && data["menu_locations"]["sidebar"]) {
+        let sideMenuID = data["menu_locations"]["sidebar"]["ID"];
+        menu = data["menu"][sideMenuID]["items"];
       }
 
-      return menu
+      return menu;
     },
 
     socialMenuData() {
-      let menu = []
-      let data = useGeneralStore().data
+      let menu = [];
+      let data = useGeneralStore().data;
 
-      if (data['menu_locations'] && data['menu_locations']['social-media']) {
-        let socialMenuID = data['menu_locations']['social-media']['ID']
-        menu = data['menu'][socialMenuID]['items']
+      if (data["menu_locations"] && data["menu_locations"]["social-media"]) {
+        let socialMenuID = data["menu_locations"]["social-media"]["ID"];
+        menu = data["menu"][socialMenuID]["items"];
       }
 
-      return menu
+      return menu;
     },
 
     footerMenuData() {
-      let data = useGeneralStore().data
-      let menu_left = []
-      let menu_right = []
+      let data = useGeneralStore().data;
+      let menu_left = [];
+      let menu_right = [];
 
       if (
-        data['menu_locations'] &&
-        data['menu_locations']['footer-menu-left']
+        data["menu_locations"] &&
+        data["menu_locations"]["footer-menu-left"]
       ) {
-        let menuIDLeft = data['menu_locations']['footer-menu-left']['ID']
-        menu_left = data['menu'][menuIDLeft]['items']
+        let menuIDLeft = data["menu_locations"]["footer-menu-left"]["ID"];
+        menu_left = data["menu"][menuIDLeft]["items"];
       }
 
       if (
-        data['menu_locations'] &&
-        data['menu_locations']['footer-menu-right']
+        data["menu_locations"] &&
+        data["menu_locations"]["footer-menu-right"]
       ) {
-        let menuIDRight = data['menu_locations']['footer-menu-right']['ID']
-        menu_right = data['menu'][menuIDRight]['items']
+        let menuIDRight = data["menu_locations"]["footer-menu-right"]["ID"];
+        menu_right = data["menu"][menuIDRight]["items"];
       }
 
       return {
         menu_left: menu_left,
         menu_right: menu_right,
-      }
+      };
     },
 
     footerRightMenuData() {
-      let data = useGeneralStore().data
-      let footerMenuID = data['menu_locations']['footer-menu-right']['ID']
+      let data = useGeneralStore().data;
+      let footerMenuID = data["menu_locations"]["footer-menu-right"]["ID"];
 
-      return data['menu'][footerMenuID]['items']
+      return data["menu"][footerMenuID]["items"];
     },
 
     footerData() {
       return {
         image:
-          'https://www.is-wireless.com/wp-content/uploads/2016/04/logo-500px.png',
-        copyright: '©2022 IS-Wireless and/or its affiliated companies.',
+          "https://www.is-wireless.com/wp-content/uploads/2016/04/logo-500px.png",
+        copyright: "©2022 IS-Wireless and/or its affiliated companies.",
         menu: {
           menu_left: {
             0: {
-              title: 'About us',
-              url: '/',
+              title: "About us",
+              url: "/",
             },
             1: {
-              title: 'Careers',
-              url: '/',
+              title: "Careers",
+              url: "/",
             },
             2: {
-              title: 'Contact',
-              url: '/',
+              title: "Contact",
+              url: "/",
             },
           },
           menu_right: {
             0: {
-              title: 'Privacy Policy',
-              url: '/',
+              title: "Privacy Policy",
+              url: "/",
             },
           },
         },
         socials: {
           twitter: {
-            url: 'https://twitter.com/is_wireless',
+            url: "https://twitter.com/is_wireless",
           },
           linkedin: {
-            url: 'https://linkedin.com/company/is-wireless',
+            url: "https://linkedin.com/company/is-wireless",
           },
           youtube: {
             url: null,
           },
         },
-        language: ['English'],
-      }
+        language: ["English"],
+      };
     },
   },
-}
+};
 </script>
 
 <style>
 html {
-  font-family: 'Lato', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Lato", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;

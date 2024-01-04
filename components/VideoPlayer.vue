19dcd321e13cb18bdd366168332145124c22e771
Kamil <kamil@webo.agency>
2024-01-04 11:07:23 +0100

    remove performance observer

--- components/VideoPlayer.vue
+++ components/VideoPlayer.vue
@@ -68,23 +68,15 @@ export default {
     }
   },
   mounted() {
-      new PerformanceObserver((entryList) => {
-    for (const entry of entryList.getEntries()) {
-      console.log('LCP candidate:', entry.startTime, entry);
-    }
-  }).observe({type: 'largest-contentful-paint', buffered: true});
-
-
-      this.player = videojs(
-        this.$refs.videoPlayer,
-        this.options
-        ).play()
-      this.$refs.videoPlayer.addEventListener('timeupdate',()=>{
-        this.playerReady()
-      },{
-        once: true
-      })
-
+    this.player = videojs(
+      this.$refs.videoPlayer,
+      this.options
+      ).play()
+    this.$refs.videoPlayer.addEventListener('timeupdate',()=>{
+      this.playerReady()
+    },{
+      once: true
+    })
   },
   methods: {
     playerReady() {

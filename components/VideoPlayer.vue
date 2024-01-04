de44f106f242a14aeef9321e104ddf75f8bab27a
Kamil <kamil@webo.agency>
2024-01-04 15:55:58 +0100

    video.js removed, video placeholder size test

--- components/VideoPlayer.vue
+++ components/VideoPlayer.vue
@@ -6,7 +6,6 @@
       loop
       muted
       loading="eager"
-      preload
       ref="videoPlayer" 
       :poster="placeholderImgFormatted"
       class="block align-top bg-black object-cover w-full [&>*]:w-full [&>*]:h-full [&>*]:object-none desktop:[&>*]:object-cover h-full"
@@ -14,23 +13,26 @@
       <source v-for="source,index in options.sources" :key="index" :src="source.src" :type="source.type" >
     </video>
 
-    <nuxt-picture v-if="placeholderImg"
-      :src="placeholderImg"
-      alt="Poster"
-      width="1920px"
-      height="768px"
+    <div v-if="placeholderImg" 
       class="h-full absolute inset-0 pointer-events-none transition duration-300"
-      :imgAttrs="{ class: 'h-full object-cover' }"
-      loading="eager"
-      preload
-      critical="true"
       ref="videoPlayerPoster"
-    />
+    >
+      <nuxt-picture
+        :src="placeholderImg"
+        alt="Poster"
+        width="1920px"
+        height="768px"
+        class="h-full w-full "
+        :imgAttrs="{ class: 'h-full object-cover' }"
+        loading="eager"
+        preload
+        critical="true"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-// import videojs from 'video.js/core.es.js'
 
 export default {
   name: 'VideoPlayer',
@@ -56,30 +58,19 @@ export default {
     
     return { placeholderImgFormatted }
   },
-  mounted() {
-    this.$nextTick(()=>{
-      // this.player = videojs(
-      //   this.$refs.videoPlayer,
-      //   this.options
-      //   ).play()
-      this.$refs.videoPlayer.addEventListener('timeupdate',()=>{
-        this.playerReady()
-      },{
-        once: true
-      })
+  mounted() {    
+    this.$refs.videoPlayer.addEventListener('timeupdate',()=>{
+      this.playerReady()
+    },{
+      once: true
     })
   },
   methods: {
     playerReady() {
       if (this.placeholderImg) {
-        this.$refs['videoPlayerPoster']?.$el.classList.add('opacity-0')
+        this.$refs.videoPlayerPoster.classList.add('opacity-0')
       }
     },
-  },
-  beforeDestroy() {
-    if (this.$refs.videoPlayer) {
-      videojs(this.$refs.videoPlayer).dispose()
-    }
   },
 }
 </script>

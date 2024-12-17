3c60405072d05a8afb7e9f4d79d9496de6be5ba8
Kamil <kamil@webo.agency>
2024-12-17 14:24:12 +0100

    organisations img load, preload links

--- nuxt.config.js
+++ nuxt.config.js
@@ -148,14 +148,6 @@ export default defineNuxtConfig({
   experimental:{
     payloadExtraction: true,
     sharedPrerenderData: true,
-    // defaults:{
-    //   nuxtLink:{
-    //     prefetchOn:{
-    //       visibility: false,
-    //       interaction: true
-    //     }
-    //   }
-    // }
   },
   vite: {
     build: {
